0a2108743e199fd92b348f95af9e11e626832ce0
Bitaan Chakrabarti <bitaan96@gmail.com>
2018-05-25 01:15:04 +0530

    Model changes

--- webcrawler_1.py
+++ webcrawler_1.py
@@ -6,7 +6,9 @@ r = requests.get(base_url)
 soup = BeautifulSoup(r.text)
  
 for story_heading in soup.find_all(class_="story-heading"): 
+
     if story_heading.a: 
         print(story_heading.a.text.replace("\n", " ").strip())
+    
     else: 
         print(story_heading.contents[0].strip())
